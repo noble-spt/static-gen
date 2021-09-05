42bff1a6f3cfb20b8a28e83baaa3cb96ca921373
Akhil Ghatiki <akhil.ghatiki@gmail.com>
2021-09-05 21:30:40 +0530

    Updates

---
+++ precache-manifest.c91df14a52d8f9b1f86d92e3193f3309.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbe4f49298583512578975dbdd4bc1a3",
+    "url": "/static-gen/index.html"
+  },
+  {
+    "revision": "992a6e267136988d8166",
+    "url": "/static-gen/static/css/2.ca8a36a0.chunk.css"
+  },
+  {
+    "revision": "abd47a153b121a03ee0f",
+    "url": "/static-gen/static/css/main.b3b2df44.chunk.css"
+  },
+  {
+    "revision": "992a6e267136988d8166",
+    "url": "/static-gen/static/js/2.1e68b6e2.chunk.js"
+  },
+  {
+    "revision": "1953a225211647eed7d9eb212830c560",
+    "url": "/static-gen/static/js/2.1e68b6e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abd47a153b121a03ee0f",
+    "url": "/static-gen/static/js/main.7a73cbcd.chunk.js"
+  },
+  {
+    "revision": "c2ce6a674b21cf544ee6",
+    "url": "/static-gen/static/js/runtime-main.4ac9968d.js"
+  },
+  {
+    "revision": "467d429c23d718322f2b1b81b35909ab",
+    "url": "/static-gen/static/media/Archive.467d429c.md"
+  },
+  {
+    "revision": "bcfaaa9365d9142aac4943ec53861f24",
+    "url": "/static-gen/static/media/Categories.bcfaaa93.md"
+  },
+  {
+    "revision": "dff3b3fc7adb7a600880dc39b4d5d06c",
+    "url": "/static-gen/static/media/Now.dff3b3fc.md"
+  },
+  {
+    "revision": "9e5cec9a13c0b4d3464db2793c15c21a",
+    "url": "/static-gen/static/media/Tags.9e5cec9a.md"
+  },
+  {
+    "revision": "9b38c8e5284b967710709cc097db0079",
+    "url": "/static-gen/static/media/home.9b38c8e5.md"
+  },
+  {
+    "revision": "c538ff4c5e4b9375da446679ac367cbd",
+    "url": "/static-gen/static/media/test1.c538ff4c.md"
+  }
+]);
